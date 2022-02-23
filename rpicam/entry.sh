0273137cf4320740f919f57acfb3a82c0f22f324
cristidragomir97 <cristidragomir97@gmail.com>
2022-02-23 13:31:52 +0000

    moving to cloud builder instance

--- rpicam/entry.sh
+++ rpicam/entry.sh
@@ -1,2 +1,6 @@
-!# /bin/bash
-
+!# /bin/
+echo 'source /opt/ros/noetic/setup.sh'
+echo 'source /root/catkin_ws/devel/setup.sh'
+echo 'cd /root/catkin_ws'
+echo 'LD_LIBRARY_PATH=/opt/ros/noetic/lib/python3" '
+echo 'roslaunch raspicam_node camerav2_1280x960.launch'
